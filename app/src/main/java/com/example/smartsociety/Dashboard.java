07ea367a8976552076536375a61854a9781c0b87
A10-g <77630063+A10-g@users.noreply.github.com>
2022-09-27 00:38:29 +0530

    auto Login side bar logout and dark mode

--- app/src/main/java/com/example/smartsociety/Dashboard.java
+++ app/src/main/java/com/example/smartsociety/Dashboard.java
@@ -3,16 +3,21 @@ package com.example.smartsociety;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
@@ -24,6 +29,8 @@ public class Dashboard extends AppCompatActivity {
     public Button vehiclebtn;
     public Button alertbtn;
     public Button societybtn;
+    public Switch switchTheme;
+
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
@@ -54,16 +61,34 @@ public class Dashboard extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch(item.getItemId()){
                     case R.id.notificationSetting:
-                        Toast.makeText(Dashboard.this, "Notification Setting", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Dashboard.this, "Notification Settings", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.changePass:
                         Toast.makeText(Dashboard.this, "Change Password", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.uiTheme:
-                        Toast.makeText(Dashboard.this, "Change Theme", Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(Dashboard.this, "Theme Changed", Toast.LENGTH_SHORT).show();
+                        switchTheme=findViewById(R.id.chTheme);
+                        switchTheme.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                            @Override
+                            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                                if(b){
+                                    getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                                }else{
+                                    getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                                }
+                            }
+                        });
                         break;
                     case R.id.logout:
-                        Toast.makeText(Dashboard.this, "Log Out", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Dashboard.this, "Logged Out", Toast.LENGTH_SHORT).show();
+                        final SharedPreferences sharedPreferences=getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor=sharedPreferences.edit();
+                        editor.clear();
+                        editor.commit();
+                        Intent intent=new Intent(Dashboard.this,LoginActivity.class);
+                        startActivity(intent);
+
                         break;
                 }
                 return true;
